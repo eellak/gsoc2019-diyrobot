edc274473b30f29316029cfe1ca68d0d6fd2d196
chronis10 <it21797@hua.gr>
2019-07-20 20:09:15 +0300

    Sudo prompt on browser interface for wifi networks, shutdown and reboot

--- code/webserver_robot_manger/index.js
+++ code/webserver_robot_manger/index.js
@@ -4,15 +4,41 @@ var fs = require("fs");
 var path = require('path');
 
 app.use(express.static("public"));
+var sudo = require('sudo-prompt');
+var options = {
+  name: 'Proteas Sudo Prompt',
+};
 
 
-app.get('/tt', function (req, res) {  
-
-   res.sendFile(path.resolve("./public/index.html"));
-   
+app.get('/wifi', function (req, res) {
+	sudo.exec('cat etc/wpa_supplicant/wpa_supplicant.conf', options,
+  	function(error, stdout, stderr) {
+    	if (error) throw error;
+	     console.log(JSON.stringify(stdout));
+	      res.send(stdout);
+  	}
+	);
 })
 
+app.get('/shutdown', function (req, res) {
+	sudo.exec('shutdown -t 0', options,
+  	function(error, stdout, stderr) {
+    	if (error) throw error;
+	     console.log("Shutdown!");
+       res.send("Shutdown");
+  	}
+	);
+})
 
+app.get('/reboot', function (req, res) {
+	sudo.exec('reboot', options,
+  	function(error, stdout, stderr) {
+    	if (error) throw error;
+	console.log("Reboot!");
+	res.send("Reboot");
+  	}
+	);
+})
 
 var server = app.listen(8080, function () {
    var host = server.address().address
